f58da588cf82f491166aa532d354f671aacc9bda
Anjana Paul <anjanapaul0614@gmail.com>
2023-12-09 21:19:27 +0530

    added ul and li nav menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,9 +10,16 @@
                 <link to='/' classname='navbar-logo'>
                     TRVL <i className='fab fa-typo3'/>
                 </link>
-                <div className="menu-icon">
+                <div className="menu-icon" onClick={handleClick}>
                     <i className={click ? 'fas fa-times': 'fas fa-bars'} />
                 </div>
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                    <li className='nav-item'>
+                        <link rel="stylesheet" href="" > 
+                            home
+                        </link>
+                    </li>
+                </ul>
             </div>
         </nav>
     </>
